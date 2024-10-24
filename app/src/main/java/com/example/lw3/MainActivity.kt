36f11844de163896dfb1d3d2c140991ecc77a4fb
Alemoring <morgunov2004@list.ru>
2024-10-25 00:42:46 +0800

    maybe final LW3

--- app/src/main/java/com/example/lw3/MainActivity.kt
+++ app/src/main/java/com/example/lw3/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.lw3
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.lw3.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    val binding
+        get() = _binding?: throw IllegalStateException("No binding!")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnShow.setOnClickListener(View.OnClickListener {
+            var text : String = "Выбор:"
+            if (binding.cbOldCar.isChecked){
+                text += " Раритетные машины;"
+            }
+            if (binding.cbNewCar.isChecked){
+                text += " Новые машины;"
+            }
+            if (binding.cbBensCar.isChecked){
+                text += " Бензиновые ввто;"
+            }
+            if (binding.cbDiselCar.isChecked){
+                text += " Дизельные авто;"
+            }
+            if (binding.cbElectricCar.isChecked){
+                text += " Электрические авто;"
+            }
+            if (binding.cbNearbyCar.isChecked){
+                text += " Авто рядом;"
+            }
+            binding.tvChoose.setText(text)
+        })
     }
 }
